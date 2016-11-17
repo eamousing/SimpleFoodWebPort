95ef334b9e52a148bcb840035e2f70ed164b9557
Erik Askov Mousing <erikmousing@gmail.com>
2016-11-17 10:38:01 -0400

    Model with Holling I predation implemented - only output to screen
    - Holling II predation not implemented
    - Output to csv not implemented

--- SimpleFoodWeb/Program.cs
+++ SimpleFoodWeb/Program.cs
@@ -16,6 +16,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace SimpleFoodWeb
 {
     class Program
@@ -31,7 +32,7 @@ namespace SimpleFoodWeb
             var holling = 1; // Do not change this parameter
 
             // set time step parameters
-            var dt = 0.02;
+            var dt = 0.05;
             var maxTime = 3000.0;
             var nStepMax = Convert.ToInt32(Math.Round(maxTime / dt));
             var timeOut = 1000.0;
@@ -39,17 +40,17 @@ namespace SimpleFoodWeb
             var nStepOutMax = Convert.ToInt32(nStepMax / nStepOut);
 
             // Initialize working arrays
-            var bmassN = new double[] { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
-            var dBmassNdt = new double[nLevels];
+            var bmassN = new double[] { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 };
+            var dBmassNDt = new double[nLevels];
 
             // Initialize output arrays
-            var bmassNOut = new double[nLevels, nStepOutMax];
-            var autotrophyOut = new double[nLevels, nStepOutMax];
-            var heterotrophyOut = new double[nLevels, nStepOutMax];
-            var predationOut = new double[nLevels, nStepOutMax];
-            var respirationOut = new double[nLevels, nStepOutMax];
-            var timOut = new double[nStepOutMax];
-            var nitrateOut = new double[nStepOutMax];
+            var bmassN_Out = new double[nLevels, nStepOutMax];
+            var autotrophy_Out = new double[nLevels, nStepOutMax];
+            var heterotrophy_Out = new double[nLevels, nStepOutMax];
+            var predation_Out = new double[nLevels, nStepOutMax];
+            var respiration_Out = new double[nLevels, nStepOutMax];
+            var time_Out = new double[nStepOutMax];
+            var nitrate_Out = new double[nStepOutMax];
 
             // Define cell volumes
             var cellVol = new double[nLevels];
@@ -66,7 +67,7 @@ namespace SimpleFoodWeb
             Console.WriteLine("[{0}]", string.Join(", ", cellVol));
 
             // Insert code that prints cell volumes to a file
-            
+
             // Initialize arrays for cell quotas
             var quotaCarbon = new double[nLevels];
             var quotaNitrate = new double[nLevels];
@@ -81,13 +82,13 @@ namespace SimpleFoodWeb
             }
 
             // Calculate nitrate cell quota for each size group based on Redfield proportions
-            for(var i = 0; i < quotaNitrate.Length; i++)
+            for (var i = 0; i < quotaNitrate.Length; i++)
             {
                 quotaNitrate[i] = quotaCarbon[i] * (16.0 / 106.0);
             }
 
             // Convert nitrate quota to micromoles N cell-1
-            for(var i = 0; i < quotaNitrate.Length; i++)
+            for (var i = 0; i < quotaNitrate.Length; i++)
             {
                 quotaNitrate[i] = quotaNitrate[i] * 1.0e6 / 1.0e15;
             }
@@ -111,19 +112,19 @@ namespace SimpleFoodWeb
             vmaxN[7] = 0.0;
 
             // Set half-saturation constant for all types
-            for(var i = 0; i < kn.Length; i++)
+            for (var i = 0; i < kn.Length; i++)
             {
                 kn[i] = 0.17 * Math.Pow(cellVol[i], 0.27);
             }
 
             // Set mortality term to be the same for all types
-            for(var i = 0; i < kResp.Length; i++)
+            for (var i = 0; i < kResp.Length; i++)
             {
                 kResp[i] = 0.03;
             }
 
             //
-            for(var i = 0; i < specVmaxN.Length; i++)
+            for (var i = 0; i < specVmaxN.Length; i++)
             {
                 specVmaxN[i] = vmaxN[i] / quotaNitrate[i];
             }
@@ -157,25 +158,25 @@ namespace SimpleFoodWeb
             var bGraz = -0.16;
 
             // Calculate maximum grazing rate for all types
-            for(var i = 0; i < gmax.GetLength(0); i++)
+            for (var i = 0; i < gmax.GetLength(0); i++)
             {
-                for(var j = 0; j < gmax.GetLength(1); j++)
+                for (var j = 0; j < gmax.GetLength(1); j++)
                 {
                     gmax[i, j] = aGraz * Math.Pow(cellVol[j], bGraz) * grazInteract[i, j];
                     eye[i, j] = i;
                 }
             }
-            
+
             // Set gmax1 = grazing eate for Holling I ((mol N l-1 day)-1)
-            for(var i = 0; i < gmax1.GetLength(0); i++)
+            for (var i = 0; i < gmax1.GetLength(0); i++)
             {
-                for(var j = 0; j < gmax1.GetLength(1); j++)
+                for (var j = 0; j < gmax1.GetLength(1); j++)
                 {
                     // Ask Mick about this.. gmax and gmax1 are identical(?)
                     gmax1[i, j] = gmax[i, j] / 1.0;
                 }
             }
-            
+
             // Print matrix to console
             //
             //var rowCount = gmax.GetLength(0);
@@ -186,7 +187,7 @@ namespace SimpleFoodWeb
             //        Console.Write(String.Format("{0}\t", gmax[row, col]));
             //    Console.WriteLine();
             //}
-            
+
             // Calculate half-saturation constant for grazing (from Ward et al. (2013))
             for (var i = 0; i < kbN.GetLength(0); i++)
             {
@@ -197,9 +198,9 @@ namespace SimpleFoodWeb
             }
 
             // Set trophic transfer efficiency
-            for(var i = 0; i < gamma.GetLength(0); i++)
+            for (var i = 0; i < gamma.GetLength(0); i++)
             {
-                for(var j = 0; j < gamma.GetLength(1); j++)
+                for (var j = 0; j < gamma.GetLength(1); j++)
                 {
                     gamma[i, j] = 0.1 * grazInteract[i, j];
                 }
@@ -213,39 +214,42 @@ namespace SimpleFoodWeb
             var imax = nLevels;
             var jmax = nLevels;
             var kmax = nLevels;
-            
+
             // Start loop over snitrate (nitrate supply rate)
-            
-            for(var sStep = 0; sStep < 10; sStep++)
+
+            for (var sStep = 0; sStep < 10; sStep++)
             {
                 var sNitrate = 0.02 + sStep * 0.06;
-                Console.WriteLine("sNitrate = {{1}}", sNitrate); // Place holder is not working
+                Console.WriteLine("sNitrate = {0}", sNitrate); // Place holder is not working
                 // Initiate time related variables within the loop
                 var nCount = 0;
                 var nOut = 0;
                 var time = 0.0;
                 var dBmassNDtOld = 0.0;
                 var dNitrateDtOld = 0.0;
+                var dNitrateDt = 0.0;
+                var prod = new double[nLevels];
+
                 // Re-initialize biomass (convert to micromoles N l-1??)
-                for(var i = 0; i < bmassN.Length; i++)
+                for (var i = 0; i < bmassN.Length; i++)
                 {
                     bmassN[i] = bmassN[i] * 1.0e-2;
                 }
-                
+
                 // Set nitrate concentration
                 var nitrate = 1.0;
 
                 // Start main time loop
-                for(var nStep = 0; nStep < nStepMax; nStep++)
+                for (var nStep = 0; nStep < nStepMax; nStep++)
                 {
                     // Evaluate autotrophy and respiration
-                    for(var i = 0; i < autotrophy.Length; i++)
+                    for (var i = 0; i < autotrophy.Length; i++)
                     {
                         autotrophy[i] = (vmaxN[i] / quotaNitrate[i]) * (nitrate / (nitrate + kn[i])) * bmassN[i];
                     }
 
                     // Maintain respiration/background loss
-                    for(var i = 0; i < respiration.Length; i++)
+                    for (var i = 0; i < respiration.Length; i++)
                     {
                         respiration[i] = kResp[i] * bmassN[i];
                     }
@@ -253,30 +257,117 @@ namespace SimpleFoodWeb
                     // Evaluate heterotrophy
                     for (var i = 0; i < imax; i++)
                     {
-                        // 
-                        var sum1 = 0.0;
-                        for (var j = 0; j < jmax; j++)
+                        if (holling == 1)
                         {
-                            sum1 = sum1 + (gamma[j, i] * gmax1[j, i] * bmassN[i] * bmassN[j]);
-                            heterotrophy[i] = sum1;
-                        }
+                            // 
+                            var sum1 = 0.0;
+                            for (var j = 0; j < jmax; j++)
+                            {
+                                sum1 = sum1 + (gamma[j, i] * gmax1[j, i] * bmassN[i] * bmassN[j]);
+                                heterotrophy[i] = sum1;
+                            }
 
-                        // Predation by all others (ask Mick)
-                        var sum2 = 0.0;
-                        for(var k = 0; k < kmax; k++)
-                        {
-                            sum2 = sum2 + (gmax1[i, k] * bmassN[k] * bmassN[i]);
-                            predation[i] = sum2;
+                            // Predation by all others (ask Mick)
+                            var sum2 = 0.0;
+                            for (var k = 0; k < kmax; k++)
+                            {
+                                sum2 = sum2 + (gmax1[i, k] * bmassN[k] * bmassN[i]);
+                                predation[i] = sum2;
+                            }
                         }
                     }
 
-                    // Write to console
-                    Console.WriteLine("[{0}]", string.Join(", ", heterotrophy));
+
+                    // To avoid numerical problems, do not let biomass decline below a low threshold value
+                    for (var i = 0; i < nLevels; i++)
+                    {
+                        if (bmassN[i] < 1.0e-25)
+                        {
+                            respiration[i] = 0.0;
+                            predation[i] = 0.0;
+                        }
+                    }
+
+                    // Evaluate rates of change
+                    dNitrateDt = -autotrophy.Sum() + sNitrate;
+
+                    for (var i = 0; i < dBmassNDt.Length; i++)
+                    {
+                        dBmassNDt[i] = autotrophy[i] + heterotrophy[i] - respiration[i] - predation[i];
+                    }
+
+                    // Store total growth
+                    for (var i = 0; i < prod.Length; i++)
+                    {
+                        prod[i] = autotrophy[i] + heterotrophy[i];
+                    }
+
+                    // Euler forward step
+                    var bmassNNew = new double[nLevels];
+                    double nitrateNew;
+
+                    for (var i = 0; i < bmassNNew.Length; i++)
+                    {
+                        bmassNNew[i] = bmassN[i] + dBmassNDt[i] * dt;
+                        bmassN[i] = bmassNNew[i];
+                    }
+
+                    nitrateNew = nitrate + dNitrateDt * dt;
+                    nitrate = nitrateNew;
+
+                    // Increment time
+                    time = time + dt;
+
+                    // Print to screen and put diagnostics into an array
+                    nCount = nCount + 1;
+                    if (nCount == nStepOut)
+                    {
+                        // Print values to screen
+                        string screenOut1 = "";
+                        screenOut1 += "Time: " + Math.Round(time, 0)
+                            + "; Nitrate: " + nitrate.ToString("0.00E0")
+                            + "; Biomass grp0:" + bmassN[0].ToString("0.00E0")
+                            + "; Biomass grp1:" + bmassN[1].ToString("0.00E0")
+                            + "; Biomass grp2:" + bmassN[2].ToString("0.00E0")
+                            + "; Biomass grp3:" + bmassN[3].ToString("0.00E0");
+
+                        Console.WriteLine(screenOut1);
+
+                        // nCount is the counter to decide whether to write output - reset here
+                        nCount = 0;
+
+                        // nOut is the position in the output array
+                        time_Out[nOut] = time;
+                        nitrate_Out[nOut] = nitrate;
+                        for (var i = 0; i < bmassN.Length; i++)
+                        {
+                            bmassN_Out[i, nOut] = bmassN[i];
+                            autotrophy_Out[i, nOut] = autotrophy[i];
+                            heterotrophy_Out[i, nOut] = heterotrophy[i];
+                            predation_Out[i, nOut] = predation[i];
+                            respiration_Out[i, nOut] = respiration[i];
+                        }
+
+                        // Increment the output counter
+                        nOut = nOut + 1;
+                    }
+                }
+                // End main time loop
+                Console.WriteLine("end off loop");
+
+
+
+                if (sStep == 0)
+                {
+                    Console.WriteLine("The first loop has now finished");
 
                 }
+
+                else
+                {
+                    Console.WriteLine("looping");
+                }
             }
-
-
         }
     }
 }
